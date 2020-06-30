60217f3b3301e3a014ed3ffff8bc7fd6e6162c52
Jayesh Rohan Singh <61769712+Conero007@users.noreply.github.com>
2020-06-30 23:55:28 +0530

    new model and max learning rate
    Increased the the number of layer in the model and decreased the max learning rate to 0.008.

--- video_capture.py
+++ video_capture.py
@@ -7,17 +7,16 @@ import torch.nn.functional as F
 import torchvision.transforms as tt
 
 
-face_classifier = cv2.CascadeClassifier('../models/haarcascade_frontalface_default.xml')
-model_state = torch.load('../models/emotion_detection_model_state.pth')
+face_classifier = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
+model_state = torch.load('./models/emotion_detection_model_state.pth')
 
-data_dir = 'D:/Programming/Projects/Deep Learning/Emotion Detection/dataset/train'
-class_labels = os.listdir(data_dir)
+class_labels = ['Angry', 'Happy', 'Neutral', 'Sad', 'Suprise']
 
 
 def conv_block(in_channels, out_channels, pool=False):
     layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
               nn.BatchNorm2d(out_channels), 
-              nn.ReLU(inplace=True)]
+              nn.ELU(inplace=True)]
     if pool: layers.append(nn.MaxPool2d(2))
     return nn.Sequential(*layers)
 
@@ -25,25 +24,41 @@ class ResNet(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
         
-        self.conv1 = conv_block(in_channels, 64)
-        self.conv2 = conv_block(64, 128, pool=True)
+        self.conv1 = conv_block(in_channels, 128)
+        self.conv2 = conv_block(128, 128, pool=True)
         self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+        self.drop1 = nn.Dropout(0.5)
         
-        self.conv3 = conv_block(128, 256, pool=True)
-        self.conv4 = conv_block(256, 512, pool=True)
-        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        self.conv3 = conv_block(128, 256)
+        self.conv4 = conv_block(256, 256, pool=True)
+        self.res2 = nn.Sequential(conv_block(256, 256), conv_block(256, 256))
+        self.drop2 = nn.Dropout(0.5)
+        
+        self.conv5 = conv_block(256, 512)
+        self.conv6 = conv_block(512, 512, pool=True)
+        self.res3 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        self.drop3 = nn.Dropout(0.5)
         
         self.classifier = nn.Sequential(nn.MaxPool2d(6), 
-                                        nn.Flatten(), 
+                                        nn.Flatten(),
                                         nn.Linear(512, num_classes))
         
     def forward(self, xb):
         out = self.conv1(xb)
         out = self.conv2(out)
         out = self.res1(out) + out
+        out = self.drop1(out)
+        
         out = self.conv3(out)
         out = self.conv4(out)
         out = self.res2(out) + out
+        out = self.drop2(out)
+        
+        out = self.conv5(out)
+        out = self.conv6(out)
+        out = self.res3(out) + out
+        out = self.drop3(out)
+        
         out = self.classifier(out)
         return out
 
@@ -76,9 +91,9 @@ while True:
             label = class_labels[pred[0]]
             
             label_position = (x, y)
-            cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+            cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 3)
         else:
-            cv2.putText(frame, 'No Face Found', (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+            cv2.putText(frame, 'No Face Found', (20, 60), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 3)
             
     cv2.imshow('Emotion Detector', frame)
 
