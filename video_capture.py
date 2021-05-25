bca62d903adad6e5b57d9383ac13401c101db179
Jayesh Rohan Singh <rohan.jayesh18@gmail.com>
2021-05-25 16:00:47 +0530

    Updated Model

--- video_capture.py
+++ video_capture.py
@@ -1,66 +1,78 @@
 import os
 import cv2
-import numpy as np
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as tt
 
 
-face_classifier = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
-model_state = torch.load('./models/emotion_detection_model_state.pth')
-
-class_labels = ['Angry', 'Happy', 'Neutral', 'Sad', 'Suprise']
+face_classifier = cv2.CascadeClassifier("./models/haarcascade_frontalface_default.xml")
+model_state = torch.load("./models/emotion_detection_model_state.pth")
+class_labels = ["Angry", "Happy", "Neutral", "Sad", "Suprise"]
 
 
 def conv_block(in_channels, out_channels, pool=False):
-    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
-              nn.BatchNorm2d(out_channels), 
-              nn.ELU(inplace=True)]
-    if pool: layers.append(nn.MaxPool2d(2))
+    layers = [
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ELU(inplace=True),
+    ]
+    if pool:
+        layers.append(nn.MaxPool2d(2))
     return nn.Sequential(*layers)
+
+
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+    ]
+    if pool:
+        layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
 
 class ResNet(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
-        
-        self.conv1 = conv_block(in_channels, 128)
-        self.conv2 = conv_block(128, 128, pool=True)
-        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+
+        self.input = conv_block(in_channels, 64)
+
+        self.conv1 = conv_block(64, 64, pool=True)
+        self.res1 = nn.Sequential(conv_block(64, 32), conv_block(32, 64))
         self.drop1 = nn.Dropout(0.5)
-        
-        self.conv3 = conv_block(128, 256)
-        self.conv4 = conv_block(256, 256, pool=True)
-        self.res2 = nn.Sequential(conv_block(256, 256), conv_block(256, 256))
+
+        self.conv2 = conv_block(64, 64, pool=True)
+        self.res2 = nn.Sequential(conv_block(64, 32), conv_block(32, 64))
         self.drop2 = nn.Dropout(0.5)
-        
-        self.conv5 = conv_block(256, 512)
-        self.conv6 = conv_block(512, 512, pool=True)
-        self.res3 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+
+        self.conv3 = conv_block(64, 64, pool=True)
+        self.res3 = nn.Sequential(conv_block(64, 32), conv_block(32, 64))
         self.drop3 = nn.Dropout(0.5)
-        
-        self.classifier = nn.Sequential(nn.MaxPool2d(6), 
-                                        nn.Flatten(),
-                                        nn.Linear(512, num_classes))
-        
+
+        self.classifier = nn.Sequential(
+            nn.MaxPool2d(6), nn.Flatten(), nn.Linear(64, num_classes)
+        )
+
     def forward(self, xb):
-        out = self.conv1(xb)
-        out = self.conv2(out)
+        out = self.input(xb)
+
+        out = self.conv1(out)
         out = self.res1(out) + out
         out = self.drop1(out)
-        
-        out = self.conv3(out)
-        out = self.conv4(out)
+
+        out = self.conv2(out)
         out = self.res2(out) + out
         out = self.drop2(out)
-        
-        out = self.conv5(out)
-        out = self.conv6(out)
+
+        out = self.conv3(out)
         out = self.res3(out) + out
         out = self.drop3(out)
-        
-        out = self.classifier(out)
-        return out
+
+        return self.classifier(out)
+
 
 model = ResNet(1, len(class_labels))
 model.load_state_dict(model_state)
@@ -72,15 +84,15 @@ while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
     labels = []
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_gray = cv2.resize(roi_gray,(48, 48), interpolation=cv2.INTER_AREA)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y : y + h, x : x + w]
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
 
-        if np.sum([roi_gray])!= 0:
+        if np.sum([roi_gray]) != 0:
             roi = tt.functional.to_pil_image(roi_gray)
             roi = tt.functional.to_grayscale(roi)
             roi = tt.ToTensor()(roi).unsqueeze(0)
@@ -89,20 +101,32 @@ while True:
             tensor = model(roi)
             pred = torch.max(tensor, dim=1)[1].tolist()
             label = class_labels[pred[0]]
-            
-            label_position = (x, y)
-            cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 3)
-        else:
-            cv2.putText(frame, 'No Face Found', (20, 60), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 3)
-            
-    cv2.imshow('Emotion Detector', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+            label_position = (x, y)
+            cv2.putText(
+                frame,
+                label,
+                label_position,
+                cv2.FONT_HERSHEY_COMPLEX,
+                2,
+                (0, 255, 0),
+                3,
+            )
+        else:
+            cv2.putText(
+                frame,
+                "No Face Found",
+                (20, 60),
+                cv2.FONT_HERSHEY_COMPLEX,
+                2,
+                (0, 255, 0),
+                3,
+            )
+
+    cv2.imshow("Emotion Detector", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
